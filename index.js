55a8e3c354beab4f10734beef66bd2080747d709
erklyn <uenesgursel@gmail.com>
2021-11-30 22:29:40 +0300

    split musteri and add needed post and get requests

--- index.js
+++ index.js
@@ -15,7 +15,12 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended:true}))
 
+
+// ------------------------------------------ DATA OKUMA -------------------------------------- \\
+
 // TEK MÜŞTERİ QUERY
+
+
 app.get('/api/musteriler/:id' , (req, res) => {
   const sqlSelect = 'SELECT * FROM musteri WHERE id= "'+req.params.id+'";'
   
@@ -23,88 +28,139 @@ app.get('/api/musteriler/:id' , (req, res) => {
     res.send(result)
   });
 });
+
+
 // TÜM MÜŞTERİLERİ ÇEKME
+
 app.get('/api/get' , (req, res) => {
   const sqlSelect = 'SELECT * FROM musteri'
   db.query(sqlSelect , (err , result) => {
+    
     res.send(result)
   })
 })
-//TEST 
-app.get('/api/:parametre/:ref' , (req, res) => {
-  const sqlSelect = 'SELECT * FROM musteri WHERE '+req.params.parametre+' = "'+req.params.ref+'"';
-  db.query(sqlSelect , (err , result) => {
-    res.send(result)
-   
-  })
-})
+
+
+
 //MÜŞTERİ ŞEHRİNE GÖRE
-app.get('/api/musteriSehir/:sehir' , (req, res) => {
-  const sqlSelect = 'SELECT * FROM musteri WHERE musteriSehir = "'+req.params.sehir+'"';
+app.get('/api/get/firmaSehir/:sehir' , (req, res) => {
+  const sqlSelect = 'SELECT * FROM musteri WHERE firmaSehir = "'+req.params.sehir+'"';
   db.query(sqlSelect , (err , result) => {
     res.send(result)
    
   })
 })
+
 //MÜŞTERİ ADINA GÖRE
-app.get('/api/musteriUnvani/:isim' , (req, res) => {
-  const sqlSelect = 'SELECT * FROM musteri WHERE musteriUnvani = "'+req.params.isim+'"'
+app.get('/api/get/firmaAdi/:isim' , (req, res) => {
+  const sqlSelect = 'SELECT * FROM musteri WHERE firmaAdi = "'+req.params.isim+'"'
   db.query(sqlSelect , (err , result) => {
     res.send(result)
     
 
   })
 })
+
 //MÜŞTERİ ARAÇ TİPİNE GÖRE
-app.get('/api/musteriAracTipi/:arac' , (req, res) => {
-  const sqlSelect = 'SELECT * FROM musteri WHERE musteriAracTipi = "'+req.params.arac+'"'
+app.get('/api/get/firmaAractipi/:arac' , (req, res) => {
+  const sqlSelect = 'SELECT * FROM musteri WHERE firmaAractipi = "'+req.params.arac+'"'
   db.query(sqlSelect , (err , result) => {
     res.send(result)
     
 
   })
 })
-//TEMSİLCİMİZ ADI İLE
-app.get('/api/firstName/:isim' , (req, res) => {
-  const sqlSelect = 'SELECT * FROM musteri WHERE firstName >= "'+req.params.isim+'"'
+
+// TEMSİLCİ ÇEKME 
+app.get('/api/get/temsilci/:id' , (req, res) => {
+  const sqlSelect = 'SELECT * FROM temsilci WHERE id = "'+req.params.id+'"'
   db.query(sqlSelect , (err , result) => {
     res.send(result)
     
+
   })
 })
-// İÇ SATIŞ Query
 
-app.get('/api/departman/:departman' , (req, res) => {
-  const sqlSelect = 'SELECT * FROM musteri WHERE departman= "'+req.params.departman+'";'
+// GÖRÜŞMELERİ ÇEKME
+app.get('/api/get/gorusme/:id' , (req, res) => {
+  const sqlSelect = 'SELECT * FROM gorusme WHERE musteriID = "'+req.params.id+'"'
   db.query(sqlSelect , (err , result) => {
     res.send(result)
+    console.log(result)
   })
 })
 
+
+
+
+
+// ------------------------------------------- DATA YAZMA ----------------------------------------- \\
 // YENİ MÜŞTERİ EKLEME
-app.post('/api/insert' , (req , res ) =>{
-  
+app.post('/api/insert/musteri' , (req , res ) =>{
+  const sqlInsertGorusme = "INSERT INTO gorusme (tarih , konusu , ozet ,musteriID , temsilciID) VALUES (?,?,?,?,?);"
+  const sqlInsertTemsilci = "INSERT INTO temsilci (adi , soyadi ,departman , mail) VALUES (?,?,?,?);"
+  const sqlInsert = "INSERT INTO musteri (firmaAdi, firmaIlgilisi , firmaAdresi , firmaMail , firmaSehir , firmaUlke,firmaTelefon,firmaAractipi , temsilciID) VALUES (?,?,?,?,?,?,?,?,?);"
 
-  const sqlInsert = "INSERT INTO musteri (firstName,lastName,departman,musteriUnvani,musteriAdresi,musteriMail,musteriSehir,musteriUlke,musteriAracTipi,musteriIlgili,musteriTelefon,gorusmeKonusu,gorusmeOzet, gorusmeTarih) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?);"
-
-  const firstName = req.body.values.firstName
-  const lastName = req.body.values.lastName
+  const firmaAdi = req.body.values.firmaAdi
+  const firmaIlgilisi = req.body.values.firmaIlgilisi
+  const firmaAdresi = req.body.values.firmaAdresi
+  const firmaMail = req.body.values.firmaMail
+  const firmaSehir = req.body.values.firmaSehir
+  const firmaUlke = req.body.values.firmaUlke
+  const firmaAracTipi = req.body.values.firmaAractipi
+  const firmaTelefon = req.body.values.firmaTelefon
+  const adi = req.body.values.adi
+  const soyadi = req.body.values.soyadi
+  const mail = 'test@test12.com'
   const departman = req.body.values.departman
-  const musteriUnvani = req.body.values.musteriUnvani
-  const musteriAdresi = req.body.values.musteriAdresi
-  const musteriMail = req.body.values.musteriMail
-  const musteriSehir = req.body.values.musteriSehir
-  const musteriUlke = req.body.values.musteriUlke
-  const musteriAracTipi = req.body.values.musteriAracTipi
-  const musteriIlgili = req.body.values.musteriIlgili
-  const musteriTelefon = req.body.values.musteriTelefon
-  const gorusmeKonusu = req.body.values.gorusmeKonusu
-  const gorusmeOzet = req.body.values.gorusmeOzet
-  const gorusmeTarih = req.body.values.gorusmeTarih
-
-  db.query(sqlInsert , [firstName,lastName,departman,musteriUnvani,musteriAdresi,musteriMail,musteriSehir,musteriUlke,musteriAracTipi,musteriIlgili,musteriTelefon,gorusmeKonusu,gorusmeOzet,gorusmeTarih],(err, result)=>{
+  const konusu = req.body.values.konusu
+  const ozet = req.body.values.ozet
+  const tarih = req.body.values.tarih
+  const musteriID = req.body.values.musteriID
+  const temsilciID = req.body.values.temsilciID
+ 
+  db.query(sqlInsert , [firmaAdi,firmaIlgilisi,firmaAdresi,firmaMail,firmaSehir,firmaUlke,firmaTelefon,firmaAracTipi,temsilciID],(err, result)=>{
     console.log(err)
   });
+  db.query(sqlInsertGorusme , [tarih , konusu , ozet, musteriID,temsilciID],(err, result)=>{
+    console.log(err)
+  });
+  db.query(sqlInsertTemsilci , [adi , soyadi, departman ,mail],(err, result)=>{
+    console.log(err)
+  });
+})
+//app.post('/api/insert/isteklifi' , (req , res ) => {
+ // GELİŞTİRME SÜRECİNDE
+
+//})
+app.post('/api/insert/temsilci' , (req , res ) => {
+  const sqlInsertMusteri = "INSERT INTO temsilci (adi , soyadi ,departman , mail) VALUES (?,?,?,?);"
+
+  const adi = req.body.values.temsilciAdi
+  const soyadi = req.body.values.temsilciSoyadi
+  const departman = req.body.values.temsilciDepartman
+  const mail = req.body.values.temsilciMail
+  
+
+  db.query(sqlInsertTemsilci , [adi , soyadi, departman ,mail],(err, result)=>{
+    console.log(err)
+  });
+
+})
+app.post('/api/insert/gorusme' ,(req , res) => {
+  const sqlInsertGorusme = "INSERT INTO gorusme (tarih , konusu , ozet ,musteriID , temsilciID) VALUES (?,?,?,?,?);"
+
+  const tarih = req.body.values.gorusmeTarih
+  const konusu = req.body.values.gorusmeKonusu
+  const ozet = req.body.values.gorusmeOzet
+  const musteriID = req.body.values.musteriID
+  const temsilciID = req.body.values.temsilciID
+
+
+  db.query(sqlInsertGorusme , [tarih , konusu , ozet, musteriID,temsilciID],(err, result)=>{
+    console.log(err)
+  });
+
 })
 
 
