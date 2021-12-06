f347ac6d4394e26688c16f68f5a10817f7a5c834
erklyn <uenesgursel@gmail.com>
2021-12-06 20:02:29 +0300

    Finished Project

---
+++ controllers/auth.js
@@ -0,0 +1,70 @@
+const { json } = require("body-parser");
+const { sign, verify } = require("jsonwebtoken");
+
+const createAccessToken = (user) => {
+  const accessToken = sign(
+    { adi: user.adi, id: user.id , departman: user.departman  },
+    "jwtsecretplschange" , {
+        expiresIn : "5m"
+    }
+  );
+
+  return accessToken;
+};
+const createRefreshToken = (user) => {
+    const refreshToken = sign(
+      { adi: user.adi, id: user.id , departman: user.departman  , isAdmin: user.isAdmin},
+      "jwtsecretplschange" 
+    );
+  
+    return refreshToken;
+  };
+  
+
+const validateToken = (req, res, next) => {
+  
+  const accessToken = req.cookies["refresh-token"];
+  if(!accessToken) {
+    res.status(404).json('No authoraization Key')
+  }
+  if (accessToken){
+    try {
+        const validToken = verify(accessToken, "jwtsecretplschange");
+        if (validToken) {
+          req.authenticated = true;
+          return next();
+        }
+      } catch (err) {
+        return res.status(400).json({ error: err });
+      }
+  }else {
+    return res.status(400).json({ error: "User not Authenticated!" });
+  }
+};
+
+const validateAdmin = (req,res,next) => {
+  
+  const accessToken = req.cookies["refresh-token"];
+  if(!accessToken) {
+    res.status(404).json('No authoraization Key')
+  }
+  if (accessToken){
+    try {
+        const validToken = verify(accessToken, "jwtsecretplschange");
+        
+        if (validToken.isAdmin) {
+          req.authenticated = true;
+          return next();
+        }
+      } catch (err) {
+        return res.status(400).json({ error: err });
+      }
+  }else {
+    return res.status(400).json({ error: "User not Authenticated!" });
+  }
+};
+
+
+
+
+module.exports = { createAccessToken, validateToken , createRefreshToken ,validateAdmin};
--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const bodyParser = require('body-parser');
 const app = express();
 const mysql = require('mysql2');
 const cors = require('cors');
-const bcrypt = require('bcrypt');
 const cookieParser = require('cookie-parser');
-const { createToken } = require('./JWT')
+const bcrypt = require('bcrypt');
+const { createRefreshToken , validateToken, validateAdmin } = require('./controllers/auth');
+const jwt = require('jsonwebtoken')
+
 //SERVER CONFİG
 const db = mysql.createPool({
     host: "localhost",
@@ -16,7 +18,12 @@ const db = mysql.createPool({
 });
 const promisePool = db.promise();
 // API'IN KULLANDIĞI MODULLER
-app.use(cors());
+
+app.use(cors({
+  origin:'http://localhost:3000',
+  preflightContinue: true,
+  credentials: true,
+}));
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(cookieParser());
@@ -31,7 +38,7 @@ app.use(cookieParser());
 // TEK MÜŞTERİ QUERY
 
 
-app.get('/api/musteriler/:id' , (req, res) => {
+app.get('/api/musteriler/:id' ,validateToken, (req, res) => {
   const sqlSelect = 'SELECT * FROM musteri WHERE id= "'+req.params.id+'";'
   
   db.query(sqlSelect , (err , result) => {
@@ -42,8 +49,8 @@ app.get('/api/musteriler/:id' , (req, res) => {
 
 // TEKLİF QUERY 
 
-app.get('/api/get/teklif/' , (req, res) => {
-  const sqlSelect = 'SELECT * FROM teklif;'
+app.get('/api/get/teklifler/' ,validateToken, (req, res) => {
+  const sqlSelect = 'SELECT * FROM teklif ORDER BY teklifTarih DESC LIMIT 20 ;'
   
   db.query(sqlSelect , (err , result) => {
     res.send(result)
@@ -52,15 +59,25 @@ app.get('/api/get/teklif/' , (req, res) => {
 
 
 //TEKLİF QUERY WİTH MUSTERİ ID 
-app.get('/api/get/teklif/:id' , (req, res) => {
+app.get('/api/get/teklif/:id' ,validateToken, (req, res) => {
   const sqlSelect = 'SELECT * FROM teklif WHERE musteriID= "'+req.params.id+'";'
   
   db.query(sqlSelect , (err , result) => {
     res.send(result)
   });
 });
+//TAMAMLANMAMIŞ VEYA RED TEKLİFLER 
+app.get('/api/get/teklifRed' ,validateToken, (req, res) => {
+  
+  const sqlSelect = 'SELECT * FROM teklif WHERE durum IN ("Beklemede" , "Red") ORDER BY teklifTarih DESC LIMIT 20'
+  
+  db.query(sqlSelect , (err , result) => {
+    
+    res.send(result)
+  });
+});
 //TEKLİF QUERY WİTH ID
-app.get('/api/get/tekteklif/:id' , (req, res) => {
+app.get('/api/get/tekteklif/:id' ,validateToken, (req, res) => {
   const sqlSelect = 'SELECT * FROM teklif WHERE id = "'+req.params.id+'";'
   
   db.query(sqlSelect , (err , result) => {
@@ -71,8 +88,8 @@ app.get('/api/get/tekteklif/:id' , (req, res) => {
 
 // TÜM MÜŞTERİLERİ ÇEKME
 
-app.get('/api/get' , (req, res) => {
-  const sqlSelect = 'SELECT * FROM musteri'
+app.get('/api/get' ,validateToken, (req, res) => {
+  const sqlSelect = 'SELECT * FROM musteri ORDER BY firmaAdi ASC'
   db.query(sqlSelect , (err , result) => {
     
     res.send(result)
@@ -82,7 +99,7 @@ app.get('/api/get' , (req, res) => {
 
 
 //MÜŞTERİ ŞEHRİNE GÖRE
-app.get('/api/get/firmaSehir/:sehir' , (req, res) => {
+app.get('/api/get/firmaSehir/:sehir' ,validateToken, (req, res) => {
   const sqlSelect = 'SELECT * FROM musteri WHERE firmaSehir = "'+req.params.sehir+'"';
   db.query(sqlSelect , (err , result) => {
     res.send(result)
@@ -91,7 +108,7 @@ app.get('/api/get/firmaSehir/:sehir' , (req, res) => {
 })
 
 //MÜŞTERİ ADINA GÖRE
-app.get('/api/get/firmaAdi/:isim' , (req, res) => {
+app.get('/api/get/firmaAdi/:isim' ,validateToken, (req, res) => {
   const sqlSelect = 'SELECT * FROM musteri WHERE firmaAdi = "'+req.params.isim+'"'
   db.query(sqlSelect , (err , result) => {
     res.send(result)
@@ -101,7 +118,7 @@ app.get('/api/get/firmaAdi/:isim' , (req, res) => {
 })
 
 //MÜŞTERİ ARAÇ TİPİNE GÖRE
-app.get('/api/get/firmaAractipi/:arac' , (req, res) => {
+app.get('/api/get/firmaAractipi/:arac' ,validateToken, (req, res) => {
   const sqlSelect = 'SELECT * FROM musteri WHERE firmaAractipi = "'+req.params.arac+'"'
   db.query(sqlSelect , (err , result) => {
     res.send(result)
@@ -111,7 +128,7 @@ app.get('/api/get/firmaAractipi/:arac' , (req, res) => {
 })
 
 // TEMSİLCİ ÇEKME 
-app.get('/api/get/temsilci/:id' , (req, res) => {
+app.get('/api/get/temsilci/:id' ,validateToken, (req, res) => {
   const sqlSelect = 'SELECT * FROM temsilci WHERE id = "'+req.params.id+'"'
   db.query(sqlSelect , (err , result) => {
     res.send(result)
@@ -121,8 +138,17 @@ app.get('/api/get/temsilci/:id' , (req, res) => {
 })
 
 // GÖRÜŞMELERİ ÇEKME
-app.get('/api/get/gorusme/:id' , (req, res) => {
+app.get('/api/get/gorusme/:id' ,validateToken, (req, res) => {
   const sqlSelect = 'SELECT * FROM gorusme WHERE musteriID = "'+req.params.id+'"'
+  db.query(sqlSelect , (err , result) => {
+    res.send(result)
+    
+  })
+})
+
+// GÖRÜŞMELERİ TARİHE GÖRE ÇEKME
+app.get('/api/get/gorusmeler' ,validateToken, (req, res) => {
+  const sqlSelect = 'SELECT * FROM gorusme ORDER BY tarih DESC LIMIT 20 '
   db.query(sqlSelect , (err , result) => {
     res.send(result)
     
@@ -133,11 +159,14 @@ app.get('/api/get/gorusme/:id' , (req, res) => {
 
 
 
+
 // ------------------------------------------- DATA YAZMA ----------------------------------------- \\
+
+
 // YENİ MÜŞTERİ EKLEME
-app.post('/api/insert/musteri' , (req , res ) =>{
+app.post('/api/insert/musteri' ,validateToken, (req , res ) =>{
   
-  const sqlInsert = "INSERT INTO musteri (firmaAdi, firmaIlgilisi , firmaAdresi , firmaMail , firmaSehir , firmaUlke,firmaTelefon,firmaAractipi , temsilciID) VALUES (?,?,?,?,?,?,?,?,?);"
+  const sqlInsert = "INSERT INTO musteri (firmaAdi, firmaIlgilisi , firmaAdresi , firmaMail , firmaSehir , firmaUlke,firmaTelefon,firmaAractipi , temsilciID, temsilciAdi) VALUES (?,?,?,?,?,?,?,?,?,?);"
 
   const firmaAdi = req.body.values.firmaAdi
   const firmaIlgilisi = req.body.values.firmaIlgilisi
@@ -148,13 +177,16 @@ app.post('/api/insert/musteri' , (req , res ) =>{
   const firmaAracTipi = req.body.values.firmaAractipi
   const firmaTelefon = req.body.values.firmaTelefon
   const temsilciID = req.body.values.temsilciID
+  const temsilciAdi = req.body.values.temsilciAdi
+
  
-  db.query(sqlInsert , [firmaAdi,firmaIlgilisi,firmaAdresi,firmaMail,firmaSehir,firmaUlke,firmaTelefon,firmaAracTipi,temsilciID],(err, result)=>{
+  db.query(sqlInsert , [firmaAdi,firmaIlgilisi,firmaAdresi,firmaMail,firmaSehir,firmaUlke,firmaTelefon,firmaAracTipi,temsilciID, temsilciAdi],(err, result)=>{
     console.log(err)
   });
 })
-app.post('/api/insert/teklif' , (req , res ) => {
-  const sqlInsertTeklif = "INSERT INTO teklif (musteriID , temsilciID ,paraBirimi ,odemeSekli , pesinatMiktari, vadeSure, teslimTarihi, teslimYeri , durum, neden , teklifNotu , birimFiyati ,adet , aracTipi ,teklifTarih) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);"
+
+app.post('/api/insert/teklif' ,validateToken, (req , res ) => {
+  const sqlInsertTeklif = "INSERT INTO teklif (musteriID , temsilciID ,temsilciAdi,paraBirimi ,odemeSekli , pesinatMiktari, vadeSure, teslimTarihi, teslimYeri , durum, neden , teklifNotu , birimFiyati ,adet , aracTipi ,teklifTarih) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);"
 
   const musteriID = req.body.values.musteriID
   const temsilciID = req.body.values.temsilciID
@@ -171,24 +203,31 @@ app.post('/api/insert/teklif' , (req , res ) => {
   const adet = req.body.values.adet
   const aracTipi = req.body.values.aracTipi
   const teklifTarih = req.body.values.teklifTarih
+  const temsilciAdi = req.body.values.temsilciAdi
+
   
 
-  db.query(sqlInsertTeklif , [musteriID , temsilciID, paraBirimi, odemeSekli, pesinatMiktari,vadeSure,teslimTarihi,teslimYeri, durum, neden, teklifNotu , birimFiyati , adet , aracTipi, teklifTarih],(err, result)=>{
+  db.query(sqlInsertTeklif , [musteriID , temsilciID,temsilciAdi, paraBirimi, odemeSekli, pesinatMiktari,vadeSure,teslimTarihi,teslimYeri, durum, neden, teklifNotu , birimFiyati , adet , aracTipi, teklifTarih],(err, result)=>{
     console.log(err)
   });
 })
 
-app.post('/api/insert/gorusme' ,(req , res) => {
-  const sqlInsertGorusme = "INSERT INTO gorusme (tarih , konusu , ozet ,musteriID , temsilciID) VALUES (?,?,?,?,?);"
 
-  const tarih = req.body.values.gorusmeTarih
+
+
+
+app.post('/api/insert/gorusme' ,validateToken,(req , res) => {
+  const sqlInsertGorusme = "INSERT INTO gorusme (tarih , konusu , ozet ,musteriID , temsilciID ,temsilciAdi) VALUES (?,?,?,?,?,?);"
+
+  const tarih = req.body.values.gorusmeTarihi
   const konusu = req.body.values.gorusmeKonusu
-  const ozet = req.body.values.gorusmeOzet
+  const ozet = req.body.values.gorusmeOzeti
   const musteriID = req.body.values.musteriID
   const temsilciID = req.body.values.temsilciID
+  const temsilciAdi = req.body.values.temsilciAdi
 
 
-  db.query(sqlInsertGorusme , [tarih , konusu , ozet, musteriID,temsilciID],(err, result)=>{
+  db.query(sqlInsertGorusme , [tarih , konusu , ozet ,musteriID,temsilciID,temsilciAdi],(err, result)=>{
     console.log(err)
   });
 
@@ -205,7 +244,11 @@ app.listen(3001 , () => {
 
 //----------------------- AUTH RELATED --------------------\\
 
-app.post('/register' , (req , res ) => {
+
+
+app.post('/auth/register' ,validateAdmin, (req , res ) => {
+
+
   const sqlInsertTemsilci = "INSERT INTO temsilci (adi , soyadi ,departman , mail , username , password ) VALUES (?,?,?,?,?,?);"
   
   const adi = req.body.values.adi
@@ -214,6 +257,8 @@ app.post('/register' , (req , res ) => {
   const mail = req.body.values.mail
   const username = req.body.values.username
   const password = req.body.values.password
+
+
   
   
   bcrypt.hash(password, 10).then((hash) => {
@@ -222,32 +267,55 @@ app.post('/register' , (req , res ) => {
       res.json('Temsilci Başarıyla Oluşturuldu.')
     });
   })
-
   
+
 
   
 
 })
 
-app.post('/login' , async (req,res) => {
+app.post('/auth/currentUser' , async(req,res) => {
+
+      const refreshToken = req.cookies["refresh-token"];
+      
+      if(refreshToken){
+          const decodedToken = await jwt.verify(refreshToken , "jwtsecretplschange")
+
+          res.send(decodedToken)
+      }else{
+        res.send(null)
+      }
+
+});
+
+app.post('/auth/login' , async (req,res) => {
   const { username , password } = req.body.values;
   const sqlSelect = 'SELECT * FROM temsilci WHERE username ="'+username+'";'
   let [user , fields] = await promisePool.query(sqlSelect);
-    
-    
 
-   if (user.length === 0){
+  const activeUser = user[0];  
+
+   if (!activeUser){
     res.status(400).json({error: 'Böyle bir kullanıcı yok!'})
    }
 
     const dbPassword = user[0].password
       bcrypt.compare(password ,dbPassword).then((match)=> {
         if(match) {
-          const token = createToken(user[0]);
-          res.cookie("jwtToken" , token , {
-            maxAge: 60*60*12*1000
+          const refreshToken = createRefreshToken(activeUser);
+          res.cookie("refresh-token", refreshToken, {
+            maxAge: 60 * 60 * 24 * 30 * 1000,
+            httpOnly: true,
           });
-          res.json('Logged in');
+          res.status(200).json({
+            username: activeUser.username,
+            id: activeUser.id,
+            departman: activeUser.departman,
+            isAdmin: activeUser.isAdmin
+          })
+
+
+          
         }else {
           res.status(400)
           .json({
@@ -257,11 +325,11 @@ app.post('/login' , async (req,res) => {
         }
       });
       
+    });
 
 
+app.get("/auth/logout", validateToken, (req, res) => {
+  res.cookie("refresh-token", '', {maxAge: 0});
 
-});
-
-app.post('/verifyAuth' , (req , res) => {
-  
+      res.status(200).json("You logged out successfully.");
 });
