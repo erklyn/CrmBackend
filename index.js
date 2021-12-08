02b5251166cba61e46bd6cd80dd72f16871f09c6
erklyn <uenesgursel@gmail.com>
2021-12-08 08:27:01 +0300

    hello

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const promisePool = db.promise();
 // API'IN KULLANDIĞI MODULLER
 
 app.use(cors({
-  origin:['http://localhost:3000','https://stupefied-pare-7c12e3.netlify.app/'],
+  origin:['http://localhost:3000','https://stupefied-pare-7c12e3.netlify.app'],
   preflightContinue: true,
   credentials: true,
 }));
